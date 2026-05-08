459b046da320ae3eca9db21713f8275b817b66d2
exynos198200 <exynos198200@gmail.com>
2026-05-08 16:05:34 +0300

    Sync src/App.tsx via App Studio Pro

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>App Studio Works! ✅</h1>
+      <p>APK собрался правильно!</p>
+    </div>
+  );
+}
